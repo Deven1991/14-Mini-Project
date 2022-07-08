80f7a34d613b755f289bde17018406409aed3d18
Melissa Deven <99757260+Deven1991@users.noreply.github.com>
2022-07-07 21:29:20 -0500

    updated home-routes.js

--- Develop/controllers/home-routes.js
+++ Develop/controllers/home-routes.js
@@ -28,30 +28,30 @@ router.get('/', async (req, res) => {
 });
 
 // GET one gallery
-router.get('/gallery/:id', async (req, res) => {
+router.get('/user/:id', async (req, res) => {
     // If the user is not logged in, redirect the user to the login page
     if (!req.session.loggedIn) {
         res.redirect('/login');
     } else {
         // If the user is logged in, allow them to view the gallery
         try {
-            const dbGalleryData = await User.findByPk(req.params.id, {
+            const dbUserData = await User.findByPk(req.params.id, {
                 include: [
                     {
-                        model: Painting,
+                        model: Project,
                         attributes: [
                             'id',
-                            'title',
-                            'artist',
-                            'exhibition_date',
-                            'filename',
+                            'name',
                             'description',
+                            'date_created',
+                            'needed_funding',
+                            'user_id'
                         ],
                     },
                 ],
             });
-            const gallery = dbGalleryData.get({ plain: true });
-            res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
+            const User = dbUserData.get({ plain: true });
+            res.render('user', { user, loggedIn: req.session.loggedIn });
         } catch (err) {
             console.log(err);
             res.status(500).json(err);
@@ -60,18 +60,18 @@ router.get('/gallery/:id', async (req, res) => {
 });
 
 // GET one painting
-router.get('/painting/:id', async (req, res) => {
+router.get('/project/:id', async (req, res) => {
     // If the user is not logged in, redirect the user to the login page
     if (!req.session.loggedIn) {
         res.redirect('/login');
     } else {
         // If the user is logged in, allow them to view the painting
         try {
-            const dbPaintingData = await Painting.findByPk(req.params.id);
+            const dbProjectData = await Project.findByPk(req.params.id);
 
-            const painting = dbPaintingData.get({ plain: true });
+            const project = dbProjectData.get({ plain: true });
 
-            res.render('painting', { painting, loggedIn: req.session.loggedIn });
+            res.render('project', { project, loggedIn: req.session.loggedIn });
         } catch (err) {
             console.log(err);
             res.status(500).json(err);
